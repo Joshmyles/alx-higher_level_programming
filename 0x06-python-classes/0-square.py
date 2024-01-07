cc33c8514e94d9af5cd324217d9b7e3faab0c998
Joshmyles <joshmyles844@gmail.com>
2024-01-07 10:46:23 -0800

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 
+"""
+Empty class representing a square
+Attributes:
+    No attributes defined
+Methods:
+    No methods defined
+"""
 class Square:
     """
     Empty class representing a square
