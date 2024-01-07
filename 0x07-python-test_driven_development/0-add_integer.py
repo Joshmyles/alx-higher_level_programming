07cb859ebbe7e12a0377a53b0569a92a5b7cc6bb
Joshmyles <joshmyles844@gmail.com>
2024-01-07 12:30:40 -0800

    Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,3 +1,35 @@
+#!/usr/bin/python3
+
+
+"""
+add_integer: function
+
+Attributes:
+    a (int or float)
+    b (int or float)
+
+Methods:
+    add_integer
+"""
+
+
 def add_integer(a, b=98):
-    try:
-        if type a == int || type 
+    """
+    Adds two integers
+
+    Parameters:
+        a (int or float): first number
+        b (int or float): second number
+
+    Returns:
+        int: The addition of a and b
+
+    Raises:
+        TypeError: if a or b is not an int or float
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer or float")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer or float")
+
+    return int(a) + int(b)
