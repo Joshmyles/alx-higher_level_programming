72e18ff77fc512f4c695964fff72bde4eb37c897
Joshmyles <joshmyles844@gmail.com>
2024-01-07 11:22:37 -0800

    Squrae with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """
+    Square class
+
+    Attributes:
+        size (int): Private instance attribute
+
+    Methods:
+        No methods
+    """
+
+    def __init__(self, size):
+        """
+        Initialize a new instance of the square
+
+        Parameters:
+            size (int): The size of the square
+        """
+        self.__size = size
