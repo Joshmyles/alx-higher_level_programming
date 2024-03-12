ff488a4074ae26e35b4f9187e6a8e426331b293c
Joshmyles <joshmyles844@gmail.com>
2024-03-11 19:52:58 -0700

    12-object.js

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+const args = process.argv.slice(2).map(Number);
+
+for (let i = 0; i < args.length; i++) {
+  if (args[i] === 12) {
+    args[i] = 89;
+  }
+}
+
+console.log(args);
