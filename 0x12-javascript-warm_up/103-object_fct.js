c88f12e6c3940cdfcf0eae635b3b6c086e5455bb
Joshmyles <joshmyles844@gmail.com>
2024-03-11 19:56:06 -0700

    103-object_fct.js

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+
+function incrementAndCall(number, theFunction) {
+  const incr = num => num + 1;
+  const incrementedNumber = incr(number);
+  theFunction(incrementedNumber);
+}
+
+module.exports = incrementAndCall;
