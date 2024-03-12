e368aab75ec3b6c2dcc9b15582e415490bcc0d41
Joshmyles <joshmyles844@gmail.com>
2024-03-11 19:55:35 -0700

    102-add_me_maybe.js

---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+function incrementAndCall(number, theFunction) {
+  const incrementedNumber = number + 1;
+  theFunction(incrementedNumber);
+}
+
+module.exports = incrementAndCall;
