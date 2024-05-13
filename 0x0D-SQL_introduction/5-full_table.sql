0f8c3dae93454dc9e4303b40162f0269a5585e47
joshmyles <joshmyles844@gmail.com>
2024-05-13 07:26:15 -0700

    5. Full description

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- script that prints full description of the first_table
+-- database name passed as an argument of the mysql command
+-- not allowed to use the DESCRIBE or EXPLAIN statements
+SHOW CREATE TABLE first_table;
