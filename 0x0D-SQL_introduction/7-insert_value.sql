5f294c4767ae56ff0bc1d38f27a03c39f2ffd821
joshmyles <joshmyles844@gmail.com>
2024-05-13 07:34:23 -0700

    7. First Add

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
 -- inserts a new row in the table first_table
 -- new row id - 89, name - Best School
 -- database name will be passed as an argument
-INSERT INTO TABLE `first_name` (`id`, `name`) VALUES (89, 'Best School');
+INSERT INTO first_name (id, name) VALUES (89, 'Best School');
