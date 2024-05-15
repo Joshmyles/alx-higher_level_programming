cc3550ae38bc81d6d3030b3bdd6c67f13b801adf
Joshmyles <joshmyles844@gmail.com>
2024-05-15 16:30:20 +0300

    4. ID can't be null

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -3,4 +3,4 @@
 -- name VARCHAR(256)
 -- if the table already exists, should not fail
 
-CREATE TABLE IF NOTE EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
