eb4a5060731defd90009f3b9fabdc60bf1f45d54
Joshmyles <joshmyles844@gmail.com>
2024-05-15 15:39:13 +0300

    12. No genre

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,10 @@
+-- a script that list all shows contained in hbtn_0d_tvshows without a genre liked
+-- each record should display tv_shows.title, tv_shows_genres.genre_id
+-- results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- you can only use one select statement
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id=tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id
