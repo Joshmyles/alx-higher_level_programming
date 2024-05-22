80e6629121645cf3138111e0cb16f782bea01996
Joshmyles <joshmyles844@gmail.com>
2024-05-22 13:24:33 +0300

    11. Genre ID for all shows

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,11 +1,12 @@
--- import database dump of hbtn_0d_tvshows 
+-- import database dump of hbtn_0d_tvshows  
+-- script that lists all shows contained in db hbtn_0d_tvshows
 -- each record should display tv_shows.title, tv_show_genres.genre_id
 -- results are sorted in ascending order by tv_shows.title, tv_show_genres.genre_id
 -- if a show doesn't have a genre, display NuLL
--- you can use select statement
+-- you can use one select statement
 
 SELECT tv_shows.title, tv_show_genres.genre_id 
 FROM tv_shows
 LEFT JOIN tv_show_genres 
 ON tv_shows.id=tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_shows_genres.genre_id;
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
