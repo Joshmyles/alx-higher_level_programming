2fff4688a7575d57d9d8174b40c68fb13cd5b144
Joshmyles <joshmyles844@gmail.com>
2024-05-22 13:19:51 +0300

    No Comedy tonight!

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -11,7 +11,7 @@ WHERE tv_shows.id NOT IN
 (
 SELECT tv_shows.id 
 FROM tv_shows
-INNER JOIN tv_shows_genre
+INNER JOIN tv_show_genres
 ON tv_shows.id=tv_show_genres.show_id
 INNER JOIN tv_genres
 ON tv_show_genres.genre_id=tv_genres.id 
