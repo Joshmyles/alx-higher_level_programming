039a7b257341a542ea45ccc1bc7a5cde99d36120
Joshmyles <joshmyles844@gmail.com>
2024-05-23 10:28:29 +0300

    9. Add

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,6 @@
 
 const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0; i < lines.length; i++) {
-  console.log(lines[i]);
+for (const element of lines) {
+  console.log(element);
 }
