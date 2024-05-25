f3b949a13e9b3c0f6fcedf40fe48093d36144358
Joshmyles <joshmyles844@gmail.com>
2024-05-25 13:00:32 +0300

    16. Add me maybe

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-exports.addMeMaybe = function (number, theFunction()) {
+exports.addMeMaybe = function (number, theFunction) {
   const incrementedNumber = number + 1;
   theFunction(incrementedNumber);
 };
