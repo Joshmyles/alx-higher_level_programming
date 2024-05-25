398905fb1778eff81712258dbce0c38a75a9ab6c
joshmyles <joshmyles844@gmail.com>
2024-05-25 04:34:07 -0700

    Standardizing

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 module.exports = class Square extends require('./5-square') {
-  constructor(size) {
+  constructor (size) {
     super(size, size);
   }
 
-  charPrint(c) {
+  charPrint (c) {
     if (c === undefined) {
       this.print();
     } else {
