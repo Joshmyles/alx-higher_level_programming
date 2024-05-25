b95383edddc9a52443e9dee8d1520cfa15d2376e
Joshmyles <joshmyles844@gmail.com>
2024-05-25 14:29:55 +0300

    6. Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+module.exports = class Square extends require('./5-square') {
+  constructor(size) {
+    super(size, size);
+  }
+
+  charPrint(c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.size; i++) console.log('C'.repeat(this.size));
+    }
+  }
+};
