cb148859b1014de16110ce08b6a702df9692ad3d
Joshmyles <joshmyles844@gmail.com>
2024-05-30 13:05:49 +0300

    0. Readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+// script that reads and prints the content of a file
+// the first argument is the file path
+// if an error occurred during reading, print error object
+
+const fs = require('fs');
+const file = process.argv[2];
+
+fs.readFile(file, 'utf-8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
