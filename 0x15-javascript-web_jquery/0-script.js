d8c98557f1d5d3dbe75906f241379d19a699f629
Joshmyles <joshmyles844@gmail.com>
2024-06-02 18:04:14 +0300

    0. No JQuery

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+/**
+ * a script that updates the text color of the <header>
+ * element to red(#FF0000)
+ * you can't use the jquery api
+ */
+
+document.querySelector('header').style.color = '#FF0000';
